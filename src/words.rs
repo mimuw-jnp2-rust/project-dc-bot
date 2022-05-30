c83e045b8956182052ec8b6d228a207227102ba6
Julia Podrażka <73885839+julia-podrazka@users.noreply.github.com>
2022-05-31 00:50:43 +0200

    Debug + format

--- src/words.rs
+++ src/words.rs
@@ -1,47 +1,41 @@
-use std::process::Output;
-use serde::Deserialize;
 use bracket_random::prelude::RandomNumberGenerator;
-use serenity::futures::future::MaybeDone::Future;
-use serenity::futures::{StreamExt, TryStreamExt};
-use tokio::task::spawn_blocking;
+use serde::Deserialize;
 
+/* Struct representing a single available word. */
 #[derive(Deserialize)]
 pub struct Word {
-    word: String,
+    pub word: String,
 }
 
+/* Struct representing all available words to guess. */
 pub struct Words {
-    words: Vec<Word>,
-}
-
-impl Word {
-    pub fn word(&self) -> String {
-        self.word.clone()
-    }
+    pub words: Vec<Word>,
 }
 
 impl Words {
     pub async fn new() -> Words {
-        let result = reqwest::get("https://raw.githubusercontent.com/mongodb-developer/bash-wordle/main/words.json")
-            .await;
+        let result = reqwest::get(
+            "https://raw.githubusercontent.com/mongodb-developer/bash-wordle/main/words.json",
+        )
+        .await;
 
         match result {
             Err(why) => {
                 println!("Error fetching data: {}", why);
-                Words { words: vec![Word { word: String::from("empty") }] }
-            }
-            Ok(response) => {
                 Words {
-                    words: response.json()
-                        .await
-                        .unwrap()
+                    words: vec![Word {
+                        word: String::from("empty"),
+                    }],
                 }
             }
+            Ok(response) => Words {
+                words: response.json().await.unwrap(),
+            },
         }
     }
 
     pub fn generate_word(&self) -> &Word {
         let mut rng = RandomNumberGenerator::new();
-        rng.random_slice_entry(&self.words).unwrap().clone()
+        rng.random_slice_entry(&self.words).unwrap()
     }
 }
