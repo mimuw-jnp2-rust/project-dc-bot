68919dfb3a4d7ca9f0cbc437f2a4b0d24f132369
Julia Podrażka <73885839+julia-podrazka@users.noreply.github.com>
2022-05-30 18:12:47 +0200

    Creating wordle game for new clients

--- src/wordle.rs
+++ src/wordle.rs
@@ -1,26 +1,27 @@
 use std::collections::HashMap;
 
-enum Result {Green, Yellow, Black}
+pub enum Result {Green, Yellow, Black}
 static DEFAULT_SIZE: u32 = 5;
 static GUESSES: u32 = 6;
 
-struct Field {
-    letter: char,
-    square: Result
+pub struct Field {
+    pub letter: char,
+    pub square: Result
 }
 
 /* Struct representing a single instance of the game. */
 pub struct Wordle {
-    word: String,
-    size: u32,
-    guesses: u32,
-    fields: HashMap<u32, Vec<Field>>
+    pub word: String,
+    pub guesses: u32,
+    pub fields: HashMap<u32, Vec<Field>>
 }
 
 impl Wordle {
-
-    fn new(size: u32) -> Wordle {
-        /* TODO randomly picking a word of given size */
-        !unimplemented!()
+    pub fn new() -> Wordle {
+        Wordle {
+            word: String::from("ABCDE"),
+            guesses: 0,
+            fields: HashMap::new(),
+        }
     }
 }
